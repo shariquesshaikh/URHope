7f6acfc1f07342d23e5ee2a4aa2a80aedb6eec84
Jino Jossy <jinojossy93@gmail.com>
2020-04-24 01:27:37 +0530

    add scripts

---
+++ scripts/add_pos.py
@@ -0,0 +1,34 @@
+import pymysql
+import csv
+
+db = pymysql.connect(host='localhost', user='root', passwd='',
+                         db='covid', charset='utf8mb4')
+c = db.cursor()
+keys = ['pin', 'officename', 'divisionname', 'regionname', 'circlename', 'taluk', 'districtname', 'statename', 'relsuboffice']
+key_map = {}
+with open("/home/jino/Downloads/URHope/india_po_data.csv") as f:
+    for i, j in enumerate(f.readlines()):
+        headers = []
+        if not i:
+            headers = j.split(",")
+            for k in keys:
+                key_map[k]=headers.index(k)
+        else:
+            j = j.split(",")
+            query = "insert into podata ("
+            vals = " values ("
+            for k,v in key_map.items():
+                query += k+", "
+                vals += "'"+j[v]+"', "
+            q = query[:-2]+") "+vals[:-2]+");"
+            print("Processing :%s" % j[key_map['pin']])
+            try:
+                c.execute(q)
+                db.commit()
+            except:
+                pass
+
+        # if i ==11:
+        #     break
+
+db.close()
