0d23697ee666b315ccb625345331fe68824be591
Jino Jossy <jinojossy93@gmail.com>
2020-04-24 10:36:39 +0530

    uncomment script code

--- scripts/add_pos.py
+++ scripts/add_pos.py
@@ -13,29 +13,29 @@ key_map = {}
 with ZipFile("./india_po_data.csv.zip", "r") as z:
     z.extractall()
 
-# with open("./india_po_data.csv") as f:
-#     for i, j in enumerate(f.readlines()):
-#         headers = []
-#         if not i:
-#             headers = j.split(",")
-#             for k in keys:
-#                 key_map[k]=headers.index(k)
-#         else:
-#             j = j.split(",")
-#             query = "insert into podata ("
-#             vals = " values ("
-#             for k,v in key_map.items():
-#                 query += k+", "
-#                 vals += "'"+j[v]+"', "
-#             q = query[:-2]+") "+vals[:-2]+");"
-#             print("Processing :%s" % j[key_map['pin']])
-#             try:
-#                 c.execute(q)
-#                 db.commit()
-#             except:
-#                 pass
+with open("./india_po_data.csv") as f:
+    for i, j in enumerate(f.readlines()):
+        headers = []
+        if not i:
+            headers = j.split(",")
+            for k in keys:
+                key_map[k]=headers.index(k)
+        else:
+            j = j.split(",")
+            query = "insert into podata ("
+            vals = " values ("
+            for k,v in key_map.items():
+                query += k+", "
+                vals += "'"+j[v]+"', "
+            q = query[:-2]+") "+vals[:-2]+");"
+            print("Processing :%s" % j[key_map['pin']])
+            try:
+                c.execute(q)
+                db.commit()
+            except:
+                pass
 
-#         # if i ==11:
-#         #     break
+        # if i ==11:
+        #     break
 os.remove("./india_po_data.csv")
 db.close()
